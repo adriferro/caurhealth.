01e6a28a819d5c89e5108629d559375fb461e8fb
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-05-15 10:57:09 +0200

    Cambiamos el formato del archivo index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Inicio</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 
@@ -11,6 +11,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body>
+    <section class="header">
+        <a href="index.php" class="logo">GrandCare</a>
+
+        <nav class="navbar">
+            <a href="index.php">Inicio</a>
+            <a href="about.php">Nosotros</a>
+            <a href="helpers.php">Cuidadores</a>
+            <a href="new.php">Nuevo Cuidador</a>
+        </nav>
+    </section>
 
 
 
