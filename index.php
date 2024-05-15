988cdcb6c93c82b81b3cfb701c1464a3927ccd84
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-05-15 12:17:44 +0200

    Arreglamos las rutas hacia los diferentes templates

--- index.php
+++ index.php
@@ -15,15 +15,30 @@
         <a href="index.php" class="logo">GrandCare</a>
 
         <nav class="navbar">
-            <a href="about.php">Nosotros</a>
-            <a href="helpers.php">Cuidadores</a>
-            <a href="new.php">Artículos</a>
+            <a href="templates/about.php">Nosotros</a>
+            <a href="templates/articles.php">Artículos</a>
+            <a href="templates/helpers.php">Cuidadores</a>
+            <a href="templates/new.php">Nuevo Cuidador</a>
         </nav>
 
         <div id="menu-btn" class="fas fa-bars"></div>
     </section>
 
 
+
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Links Rápidos</h3>
+                <a href="templates/about.php"><i class="fa-solid fa-angle-right"></i> Nosotros</a>
+                <a href="templates/articles.php"><i class="fa-solid fa-angle-right"></i> Artículos</a>
+                <a href="templates/helpers.php"><i class="fa-solid fa-angle-right"></i> Cuidadores</a>
+                <a href="templates/new.php"><i class="fa-solid fa-angle-right"></i> Nuevo Cuidador</a>
+            </div>
+
+        </div>
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="scripts/main.js"></script>
