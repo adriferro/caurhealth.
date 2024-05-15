8332bf4655074ef377a0ceaca600f251d516ad51
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-05-15 13:35:34 +0200

    Agregamos el header y el footer a los demás templates

---
+++ templates/new.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuevo</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+    <section class="header">
+        <a href="index.php" class="logo">GrandCare</a>
+
+        <nav class="navbar">
+            <a href="templates/about.php">Nosotros</a>
+            <a href="templates/articles.php">Artículos</a>
+            <a href="templates/helpers.php">Cuidadores</a>
+            <a href="templates/new.php">Nuevo cuidador</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+
+
+    <div class="heading">
+        <h1>Únete a nosotros!</h1>
+    </div>
+
+
+
+
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Links Rápidos</h3>
+                <a href="templates/about.php"><i class="fa-solid fa-angle-right"></i> Nosotros</a>
+                <a href="templates/articles.php"><i class="fa-solid fa-angle-right"></i> Artículos</a>
+                <a href="templates/helpers.php"><i class="fa-solid fa-angle-right"></i> Cuidadores</a>
+                <a href="templates/new.php"><i class="fa-solid fa-angle-right"></i> Nuevo cuidador</a>
+            </div>
+
+            <div class="box">
+                <h3>Links Extras</h3>
+                <a href="#"><i class="fa-solid fa-angle-right"></i> Dudas / Sugerencias</a>
+                <a href="#"><i class="fa-solid fa-angle-right"></i> Regulación </a>
+                <a href="#"><i class="fa-solid fa-angle-right"></i> Política de privacidad</a>
+                <a href="#"><i class="fa-solid fa-angle-right"></i> Términos de uso</a>
+            </div>
+            
+            <div class="box">
+                <h3>Contacto</h3>
+                <a href="#"><i class="fa-solid fa-phone"></i> 999 999 999 </a>
+                <a href="#"><i class="fa-solid fa-envelope"></i> grandcare@gmail.com </a>
+                <a href="#"><i class="fab fa-facebook-f"></i> grandcare </a>
+                <a href="#"><i class="fab fa-instagram"></i> grandcare </a>
+            </div>
+        </div>
+
+        <div class="credit"> Creado por <span>Adrián Fernández</span> | Todos los derechos reservados! </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="scripts/main.js"></script>
+</body>
+</html>
