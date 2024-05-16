9b9391ef526ee81bcd168e47edcbc3cd5d51b4b5
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-05-16 12:43:52 +0200

    Insertamos la logica de paginacion y autoplay al slider

--- scripts/main.js
+++ scripts/main.js
@@ -12,9 +12,19 @@ window.onscroll  = () => {
 };
 
 var swiper = new Swiper(".home-slider", {
-    loop:true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop:true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
