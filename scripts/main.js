ce35f0a0e23cf0aba3938508c0b0f4c1ad4ad8f7
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-05-20 10:45:20 +0200

    Agregamos la logica de envio de formulario de union

--- scripts/main.js
+++ scripts/main.js
@@ -43,3 +43,17 @@ loadMoreBtn.onclick = () => {
     loadMoreBtn.style.display = 'none';
   }
 }
+
+function sendEmail(){
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "grandcare@gmail.com",
+    Password : "password",
+    To : 'grandcare@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : "Solicitud de nuevo cuidador",
+    Body : "Nombre: " + document.getElementById("name").value + "<br> Apellido: " + document.getElementById("surname").value + "<br> Email: " + document.getElementById("email").value + "<br> Teléfono: " + document.getElementById("phone").value + "<br> Interno: " + document.getElementById("intern").value + "<br> Fecha nac.: " + document.getElementById("birth").value
+}).then(
+  message => alert("¡Formulario enviado correctamente!")
+);
+}
