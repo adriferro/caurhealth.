57bdbb25398022660fc1fa87a67d21d141f7f117
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-05-20 12:25:47 +0200

    Terminamos la logica del envio de datos del formulario de union

--- php/new_form.php
+++ php/new_form.php
@@ -8,5 +8,13 @@
         $phone = $_POST['phone'];
         $intern = $_POST['intern'];
         $birth = $_POST['birth'];
+
+        $request = "insert into new_form(name, surname, email, phone, intern, birth) values ('$name','$surname','$email','$phone','$intern','$birth')";
+
+        mysqli_query($connection, $request);
+
+        header('location:new.php');
+    }else{
+        echo 'Error de solicitud. Por favor, intente de nuevo.';
     }
 ?>
