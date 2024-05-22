f1586ba9b29177ab49b9193168ffa90fab7384da
adriferro <a.fernandezrodriguez2003@gmail.com>
2024-05-22 13:25:55 +0200

    Arreglamos la conexion con la base de datos

--- php/login/config.php
+++ php/login/config.php
@@ -1,3 +1,3 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'user_db') or die('Fallo de conexión.');
+    $conn = mysqli_connect('localhost','root','','user_db') or die('Fallo de conexión.');
 ?>
